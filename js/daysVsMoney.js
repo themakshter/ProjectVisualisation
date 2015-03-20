6d460a3ef1d943811dd234f331d76a89893a6f02
Mohammad Ali Khan <mohammad_ali93@live.com>
2015-03-20 14:38:53 +0400

    change canvas size

--- js/daysVsMoney.js
+++ js/daysVsMoney.js
@@ -1,6 +1,6 @@
 var margin = {top: 20, right: 20, bottom: 30, left: 40},
-    width = 1400 - margin.left - margin.right,
-    height = 600 - margin.top - margin.bottom;
+    width = 960 - margin.left - margin.right,
+    height = 500 - margin.top - margin.bottom;
 
 var x,y,xAxis,yAxis,zoom,svg;
 var color = d3.scale.category20();
